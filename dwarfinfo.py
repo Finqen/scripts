483033ac23427d4cd7f6e2051d4e65673bc077f2
Julian Finkenzeller <julian.finkenzeller@outlook.de>
2025-04-27 17:54:16 +0200

    - edited query

--- dwarfinfo.py
+++ dwarfinfo.py
@@ -57,7 +57,7 @@ def get_srcinfo_db(path):
                FROM binary_functions
                WHERE binary_id = (SELECT binary_id
                    FROM binaries
-               WHERE compileopt = '00000' and relpath = '/usr/{path}');""".format(path=path)
+               WHERE compileopt = '00000' and relpath = 'usr/{path}');""".format(path=path)
     function_container = []
     with conn.cursor() as cur:
         cur.execute(query)
