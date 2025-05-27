c544abdf74b14dcd56f73a588914dcc2e0aaab25
Julian Finkenzeller <julian.finkenzeller@outlook.de>
2025-05-27 18:29:39 +0200

    - removed limit

--- run_dwarfinfo.py
+++ run_dwarfinfo.py
@@ -19,7 +19,7 @@ def get_package_info_db():
     query = """SELECT b.pkg, b.abspath
                FROM binaries b
                WHERE b.compileopt = '00000'
-               ORDER BY b.pkg LIMIT 1;"""
+               ORDER BY b.pkg;"""
     packackge_container = []
     with conn.cursor() as cur:
         cur.execute(query)
