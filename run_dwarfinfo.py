2266ce41bb282c96c15181a13fa7785b7b16a6ee
Julian Finkenzeller <julian.finkenzeller@outlook.de>
2025-06-06 17:35:26 +0200

    - added csv - change for O1

--- run_dwarfinfo.py
+++ run_dwarfinfo.py
@@ -20,7 +20,7 @@ def get_package_info_db():
 
     query = """SELECT b.pkg, b.abspath, b.binary_id, b.relpath
                FROM binaries b
-               WHERE b.compileopt = '00000'
+               WHERE b.compileopt = '00001'
                ORDER BY b.pkg;"""
     packackge_container = []
     with conn.cursor() as cur:
