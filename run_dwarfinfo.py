4c696ecf78d56163fec60376293a3ccc9d5114e6
Julian Finkenzeller <julian.finkenzeller@outlook.de>
2025-06-25 11:00:48 +0200

    - limit 10

--- run_dwarfinfo.py
+++ run_dwarfinfo.py
@@ -23,7 +23,7 @@ def get_package_info_db(compile_opt):
     query = """SELECT b.pkg, b.abspath, b.binary_id, b.relpath
                FROM binaries b
                WHERE b.compileopt = '{compile_opt}'
-               ORDER BY b.pkg;""".format(compile_opt=compile_opt)
+               ORDER BY b.pkg LIMIT 10;""".format(compile_opt=compile_opt)
     packackge_container = []
     with conn.cursor() as cur:
         cur.execute(query)
